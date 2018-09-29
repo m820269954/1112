84db9c52d2d43fab63e53efa5bdc8d6ce2d951e8
m820269954 <37107636+m820269954@users.noreply.github.com>
2018-09-29 12:59:28 +0800

    Revert "Revert "Static Stack Function""
    This reverts commit 2deb9f9e95e5f0663757f6b0a4c5c1f9befbdcac.

--- StaticStack/main.c
+++ StaticStack/main.c
@@ -6,24 +6,48 @@ typedef struct {
     int top;
     int bottom;
 }Stack;
+
 void InitStack(Stack* S){
     S->top=-1;
     S->bottom=-1;
 }
 //push
 void Push(Stack* S,int data){
-    if(S->top==MaxSize){
+    if(S->top==MaxSize-1){
         printf("Stack OverFlow!\n");
         return;
     }else{
         S->top++;
-        S->stack_array[top]=data;
+        S->stack_array[S->top]=data;
     }
 }
+int Pop(Stack *S){
+    if(S->top==-1)
+        printf("Empty Stack!\n");
+    else{
+        int temp;
+        temp=S->stack_array[S->top];
+        S->top--;
+        return temp;
+    }
+}
+//print a stack
+void PrintStack(Stack *S){
+    int i=0;
+    for(i=0;i<=S->top;i++)
+        printf("%d ",S->stack_array[i]);
+}
+
 int main()
 {
+    int i=0;
     Stack S;
     InitStack(&S);
-
+    for(i=0;i<50;i++)
+        Push(&S,i);
+    PrintStack(&S);
+    for(i=0;i<51;i++)
+        Pop(&S);
+    PrintStack(&S);
     return 0;
 }
